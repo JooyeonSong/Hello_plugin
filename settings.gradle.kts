f45a60eed21b71b84f7238ce25ffeae42486d331
GitHub Action <action@github.com>
2020-07-14 07:24:34 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Hello_plugin"
--- src/main/kotlin/com/github/jooyeonsong/helloplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jooyeonsong/helloplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jooyeonsong.helloplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jooyeonsong.helloplugin.MyBundle
 
 class MyApplicationService {
 
